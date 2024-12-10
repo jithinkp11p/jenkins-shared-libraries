eec0506638db9fedbc3b6e105f3da6d4d3a12048
jithinkp11p <81610302+jithinkp11p@users.noreply.github.com>
2024-12-10 11:23:04 +0530

    Delete vars/vars directory

--- vars/vars/docker_build.groovy
+++
@@ -1,5 +0,0 @@
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  echo "Build the docker image"
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
-  echo "Build success"
-}
